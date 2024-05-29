aef88fb5fc04906b0e59df0eca5b012d8f685845
HoangMinhNg <ngminh3004@gmail.com>
2024-05-29 12:54:25 +0700

    update UI login

--- frontend/src/Component/LoginModal/index.tsx
+++ frontend/src/Component/LoginModal/index.tsx
@@ -1,65 +1,29 @@
-import { useContext, useState } from "react";
-import { loginByUsernamePassword } from "../../api/login";
-import { UserContext } from "../../context/userContext";
-import { useNavigate } from "react-router-dom";
-import toast from "react-hot-toast";
+import { useState } from "react";
+import AdminLogin from "./AdminLogin";
+import UserLogin from "./UserLogin";
+
+interface TabProps {
+  id: string;
+  label: string;
+  content: React.ReactNode;
+}
+
+const tabs: TabProps[] = [
+  {
+    id: "profile",
+    label: "Admin Login",
+    content: <AdminLogin />,
+  },
+  {
+    id: "dashboard",
+    label: "User Login",
+    content: <UserLogin />,
+  },
+];
 
 const LoginModal: React.FC = () => {
-  const { login } = useContext(UserContext);
-  const navigate = useNavigate();
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const handleSubmit = () => {
-    const loginStaff = async () => {
-      if (email === "" || password === "") {
-        toast.error("Please input all fields.", {
-          duration: 2000,
-        });
-      } else {
-        try {
-          const response = await loginByUsernamePassword(email, password);
-          const responseData = response?.data;
-          const user: LoginedUser = {
-            accountId: responseData?.accountId,
-            email: responseData?.email,
-            isLoginWithGmail: responseData?.isLoginWithGmail,
-            isNewAccount: responseData?.isNewAccount,
-            name: responseData?.name,
-            roleId: responseData?.roleId,
-            status: responseData?.status,
-            token: responseData?.token,
-            username: responseData?.username,
-          };
-          if (responseData?.token) {
-            login(user, responseData?.token);
-          }
-          if (user.roleId === 1) {
-            navigate("/admin");
-          } else if (user.roleId === 2) {
-            navigate("/owner");
-          } else if (user.roleId === 3) {
-            navigate("/member");
-          }
-        } catch (error: any) {
-          if (error.message.includes("401")) {
-            toast.error("Invalid username or password", { duration: 2000 });
-          } else {
-            toast.error("An error occurred. Please try again.", {
-              duration: 2000,
-            });
-          }
-        }
-      }
-    };
-    loginStaff();
-  };
+  const [activeTab, setActiveTab] = useState<string>(tabs[0].id);
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLFormElement>) => {
-    if (event.key === "Enter") {
-      event.preventDefault(); // Prevent form submission
-      handleSubmit(); // Trigger login action
-    }
-  };
   return (
     <>
       <div className="relative p-4 w-full max-w-md max-h-full">
@@ -70,78 +34,46 @@ const LoginModal: React.FC = () => {
             </h3>
           </div>
           <div className="p-4 md:p-5">
-            <form className="space-y-4" onKeyDown={handleKeyDown}>
-              <div>
-                <label className="block mb-2 text-lg font-medium text-gray-900 dark:text-white text-start">
-                  Your email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  placeholder="name@company.com"
-                  required
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                />
-              </div>
-              <div>
-                <label className="block mb-2 text-lg font-medium text-gray-900 dark:text-white text-start">
-                  Your password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                  required
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                />
-              </div>
-              <div className="flex justify-between">
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      id="remember"
-                      type="checkbox"
-                      value=""
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                      required
-                    />
-                  </div>
-                  <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                    Remember me
-                  </label>
+            <ul
+              className="flex flex-wrap items-center justify-center -mb-px text-sm font-medium text-center bg-white px-4 w-full"
+              role="tablist"
+            >
+              {tabs.map((tab) => (
+                <li key={tab.id} className="w-1/2" role="presentation">
+                  <button
+                    className={`inline-block p-4 rounded-t-lg w-full ${
+                      activeTab === tab.id
+                        ? "border-b-2 border-black font-semibold dark:border-gray-700 dark:text-gray-200"
+                        : "hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 font-semibold"
+                    }`}
+                    id={`${tab.id}-tab`}
+                    data-tabs-target={`#${tab.id}`}
+                    type="button"
+                    role="tab"
+                    aria-controls={tab.id}
+                    aria-selected={activeTab === tab.id}
+                    onClick={() => setActiveTab(tab.id)}
+                  >
+                    {tab.label}
+                  </button>
+                </li>
+              ))}
+            </ul>
+            <div id="default-tab-content" className="p-4 md:p-5">
+              {tabs.map((tab) => (
+                <div
+                  key={tab.id}
+                  className={`dark:bg-gray-800 ${
+                    activeTab === tab.id ? "block" : "hidden"
+                  }`}
+                  id={tab.id}
+                  role="tabpanel"
+                  aria-labelledby={`${tab.id}-tab`}
+                >
+                  {tab.content}
                 </div>
-                <a
-                  href="#"
-                  className="text-sm text-gray-900 hover:underline dark:text-blue-500"
-                >
-                  Lost Password?
-                </a>
-              </div>
-              <button
-                type="button"
-                onClick={handleSubmit}
-                className="w-full text-white bg-slate-950 hover:bg-slate-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Login to your account
-              </button>
-              <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                Not registered?{" "}
-                <a
-                  href="#"
-                  className="text-blue-700 hover:underline dark:text-blue-500"
-                >
-                  Create account
-                </a>
-              </div>
-            </form>
+              ))}
+            </div>
           </div>
         </div>
       </div>
