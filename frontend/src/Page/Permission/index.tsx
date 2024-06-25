d79bb759633f72fb249350dda0439336f70d4242
HoangMinhNg <ngminh3004@gmail.com>
2024-06-25 11:57:42 +0700

    update login function add firebase message token

--- frontend/src/Page/Permission/index.tsx
+++ frontend/src/Page/Permission/index.tsx
@@ -1,8 +1,21 @@
 const PermissionPage = () => {
   return (
     <div>
-      <h1 style={{fontWeight: "bold",fontSize: "24px"}}>Permission Denied</h1>
+      <h1 style={{ fontWeight: "bold", fontSize: "24px" }}>
+        Permission Denied
+      </h1>
       <p>Sorry, you don't have permission to access this page.</p>
+      <p>
+        Click <span> </span>
+        <a
+          href="/"
+          className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          here
+        </a>
+        <span> </span>
+        to return homepage
+      </p>
     </div>
   );
 };
