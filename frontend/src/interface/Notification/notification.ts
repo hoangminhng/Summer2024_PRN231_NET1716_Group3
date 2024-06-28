7b22a54ff431ea35bb65d8d92c4da999924a42c7
HoangMinhNg <ngminh3004@gmail.com>
2024-06-28 16:16:11 +0700

    init notification page for member, update UI notification dropdown

--- frontend/src/interface/Notification/notification.ts
+++ frontend/src/interface/Notification/notification.ts
@@ -1,8 +1,11 @@
 interface notification {
-    notificationId: number;
-    notificationType: number;
-    accountReceiveId: number;
-    title: string;
-    body: string;
-    dateCreated: Date;
-  }
+  notification_id: number;
+  account_notice_id: number;
+  receive_account_id: number;
+  notification_text: string;
+  create_date: Date;
+  notification_type: number;
+  title: string;
+  forward_to_path: string;
+  is_read: boolean;
+}
